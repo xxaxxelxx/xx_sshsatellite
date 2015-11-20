0195de920d4a82368f8ee7af9a2f26f51883e266
xxaxxelxx <x@axxel.net>
2015-11-20 11:26:47 +0100

    updated

--- synctool.sh
+++ synctool.sh
@@ -16,11 +16,25 @@ while true; do
 	echo "$M.xspf" >> files2sync
 	echo "$M.vclt" >> files2sync
     done
-    rsync -a --stats --files-from=files2sync -e 'ssh -p 65522 -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no' depot@$LOADBALANCER_ADDR:/depot/ /usr/share/icecast2/web/ | grep -i 'transferred' | grep -w '0' > /dev/null
+    rsync -a --stats --files-from=files2sync -e 'ssh -p 65522 -q -o ConnectTimeout=10 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no' depot@$LOADBALANCER_ADDR:/depot/ /usr/share/icecast2/web/ | grep -i 'transferred' | grep -w '0' > /dev/null
     if [ $? -ne 0 ]; then
 	touch /tmp/icecast.hup.sem
     fi
 
+    for F_LOG in /var/log/icecast2/*.log.*[[:digit:]]; do
+	test -r $F_LOG || continue
+	echo $(basename $F_LOG) | grep -i error > /dev/null && rm -f $F_LOG && continue
+	gzip $F_LOG > /dev/null
+    done
+    for GZ_LOG in /var/log/icecast2/*.log.*.gz; do
+	test -r $GZ_LOG || continue
+	GZ_LOG_NEW="$(dirname $GZ_LOG)/$(md5sum $GZ_LOG | awk '{print $1}').$(basename $GZ_LOG)"
+	mv -f $GZ_LOG $GZ_LOG_NEW
+	scp -P 65522 -q -o ConnectTimeout=10 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $GZ_LOG_NEW depot@$LOADBALANCER_ADDR:/depot/ > /dev/null
+	if [ $? -eq 0 ]; then
+	    rm -f $GZ_LOG_NEW
+	fi
+    done
     sleep $LOOP_SEC
 done
 
